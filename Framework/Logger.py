6bdffbb128c9b7d713d572c315eadbcf40697acb
feldt <alcid3srz@gmail.com>
2024-12-03 21:56:50 -0600

    add: loggin class and meth for journal application

---
+++ Framework/Logger.py
@@ -0,0 +1,19 @@
+
+from datetime import date, datetime
+import os
+
+
+class Journal():
+    def __init__(self):
+        self
+        
+    def InsertToLog(self, meth: str, msg: str):
+        if os.path.exists('.\\Log\\'):
+            print('exists')
+        else:
+            os.makedirs('.\\Log\\')
+            
+        base_path: str = os.path.dirname(os.path.abspath(__file__))
+        log_file: str = '\\Log\\' + str(date.today()) + '.log'
+        with open(log_file, "a", encoding="utf_8") as f:
+            print(f" {datetime.now()} | {meth}: --> {msg}", file=f)
